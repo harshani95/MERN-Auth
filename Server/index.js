fdcc4451c3babad61a71d33212358f4c05cf1c47
harshani <ruwanthikapemarathna@gmail.com>
2025-01-06 12:17:23 +0530

    Created test route

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require('./routes/UserRoute');
 
 dotenv.config();
 
@@ -19,3 +20,5 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use('/api/v1/user', userRoute);
