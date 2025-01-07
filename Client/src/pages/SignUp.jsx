5ffb6ae4b5ab86d1ce9337a440c83f36eaa5800b
harshani <ruwanthikapemarathna@gmail.com>
2025-01-07 16:28:47 +0530

    Complete SignUp functionality

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,17 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/auth/signup",
+        formData
+      );
+      console.log(response);
+      alert("User created successfully");
+      navigate("/signin");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="p-6 max-w-lg mx-auto mt-5 mb-2 bg-white rounded-lg shadow-lg">
         <h1 className="text-3xl text-center font-semibold mb-5">Sign Up</h1>
-        <form className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <input
             type="text"
             id="username"
             name="username"
             placeholder="Username"
             className="bg-slate-100 p-3 rounded-lg mb-4"
+            onChange={handleChange}
           />
           <input
             type="email"
@@ -19,6 +45,7 @@ const SignUp = () => {
             name="email"
             placeholder="Email"
             className="bg-slate-100 p-3 rounded-lg mb-4"
+            onChange={handleChange}
           />
           <input
             type="password"
@@ -26,6 +53,7 @@ const SignUp = () => {
             name="password"
             placeholder="Password"
             className="bg-slate-100 p-3 rounded-lg mb-4"
+            onChange={handleChange}
           />
           <button className="bg-slate-800 text-white p-3 rounded-lg uppercase font-semibold mb-1 mt-3 hover:bg-slate-700 transition-colors duration-300">
             Sign Up
