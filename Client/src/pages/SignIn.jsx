1b5d8bb37758e6f0784c786fcea3d3df8f8d7945
harshani <ruwanthikapemarathna@gmail.com>
2025-01-07 21:16:47 +0530

    Created SignIn page functionality

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/auth/signin",
+        formData
+      );
+      console.log(response);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className="p-6 max-w-lg mx-auto mt-5 mb-2 bg-white rounded-lg shadow-lg">
         <h1 className="text-3xl text-center font-semibold mb-5">Sign In</h1>
-        <form className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <input
             type="email"
             id="email"
             name="email"
             placeholder="Email"
             className="bg-slate-100 p-3 rounded-lg mb-4"
+            onChange={handleChange}
           />
           <input
             type="password"
@@ -19,7 +44,9 @@ const SignIn = () => {
             name="password"
             placeholder="Password"
             className="bg-slate-100 p-3 rounded-lg mb-4"
+            onChange={handleChange}
           />
+
           <button className="bg-slate-800 text-white p-3 rounded-lg uppercase font-semibold mb-1 mt-3 hover:bg-slate-700 transition-colors duration-300">
             Login
           </button>
