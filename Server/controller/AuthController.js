9547f398dca93df801a7037e82da8dba9abfbf04
harshani <ruwanthikapemarathna@gmail.com>
2025-01-07 11:00:17 +0530

    Added function to handle errors

--- Server/controller/AuthController.js
+++ Server/controller/AuthController.js
@@ -1,17 +1,17 @@
 const userSchema = require('../models/UserSchema');
 const bcrypt = require('bcryptjs');
 
-const signup = async(req, res) => {
+const signup = async(req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new userSchema({username, email, password:hashedPassword});
-    
+
  try{
     await user.save();
     res.status(200).json({message: 'User created successfully'});
  }
  catch(e){
-    res.status(400).json({error: e.message});
+    next(error);
  }  
 }
 
