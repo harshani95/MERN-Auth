53c95361378fff988b23b872893c21ef10c8e6ae
harshani <ruwanthikapemarathna@gmail.com>
2025-01-07 13:38:18 +0530

    Completed SignIn & SignUp UI

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <div className="p-6 max-w-lg mx-auto mt-5 mb-2 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl text-center font-semibold mb-5">Sign In</h1>
+        <form className="flex flex-col">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="bg-slate-100 p-3 rounded-lg mb-4"
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            className="bg-slate-100 p-3 rounded-lg mb-4"
+          />
+          <button className="bg-slate-800 text-white p-3 rounded-lg uppercase font-semibold mb-1 mt-3 hover:bg-slate-700 transition-colors duration-300">
+            Login
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Don&apos;t have an Account? </p>
+          <Link to="/signup">
+            <span className="text-blue-500 hover:underline cursor-pointer">
+              Register Here
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,5 +1,47 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="p-6 max-w-lg mx-auto mt-5 mb-2 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl text-center font-semibold mb-5">Sign Up</h1>
+        <form className="flex flex-col">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            className="bg-slate-100 p-3 rounded-lg mb-4"
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="bg-slate-100 p-3 rounded-lg mb-4"
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            className="bg-slate-100 p-3 rounded-lg mb-4"
+          />
+          <button className="bg-slate-800 text-white p-3 rounded-lg uppercase font-semibold mb-1 mt-3 hover:bg-slate-700 transition-colors duration-300">
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Have an account?</p>
+          <Link to="/signin">
+            <span className="text-blue-500 hover:underline cursor-pointer">
+              Sign In
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
