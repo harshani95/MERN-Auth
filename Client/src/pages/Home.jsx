59076cee9add0492bb1864e05a5ce5c4a81c6337
harshani <ruwanthikapemarathna@gmail.com>
2025-01-18 11:31:18 +0530

    Completed home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+//import authImage from "../assets/auth.jpg";
+import AppAuth_logo from "../assets/AppAuth_logo.svg";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="mx-w-lg mx-auto">
+        <h1 className="text-4xl font-bold text-center mt-10">
+          {" "}
+          Welcome to the Auth Home
+        </h1>
+        <img
+          src={AppAuth_logo}
+          alt="auth"
+          className="object-cover mx-auto mt-10"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
